4d6f83225c80f4799d03fb8b5945a87202b53a73
Keanu Tautuaa <ktautuaa@e1z3r6p22.42.us.org>
2019-10-17 14:22:06 -0700

    update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,30 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktautuaa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pgobeil- <pgobeil-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/07 13:08:21 by ktautuaa          #+#    #+#             */
-/*   Updated: 2019/10/14 12:33:25 by ktautuaa         ###   ########.fr       */
+/*   Created: 2019/05/14 12:50:27 by pgobeil-          #+#    #+#             */
+/*   Updated: 2019/10/17 14:21:29 by ktautuaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 
-void	ft_putnbr_fd(int nb, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	if (nb == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (nb < 0)
+	unsigned int nb;
+
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-nb, fd);
+		n = -n;
 	}
-	else if (nb >= 10)
+	nb = n;
+	if (nb > 9)
 	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putchar_fd(nb % 10 + '0', fd);
+		ft_putnbr_fd((nb / 10), fd);
+		ft_putnbr_fd((nb % 10), fd);
 	}
 	else
-		ft_putchar_fd(nb + '0', fd);
+	{
+		ft_putchar_fd((nb + '0'), fd);
+	}
 }
