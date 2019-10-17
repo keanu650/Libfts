145c265b09362f53518fb9bf5cf03b5d40b3f9b1
Keanu Tautuaa <ktautuaa@e1z3r6p22.42.us.org>
2019-10-17 13:27:01 -0700

    check memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -5,20 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktautuaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/04 12:20:06 by ktautuaa          #+#    #+#             */
-/*   Updated: 2019/10/14 12:27:38 by ktautuaa         ###   ########.fr       */
+/*   Created: 2019/10/17 13:26:24 by ktautuaa          #+#    #+#             */
+/*   Updated: 2019/10/17 13:26:33 by ktautuaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void	*ft_memalloc(size_t size)
+void		*ft_memalloc(size_t size)
 {
 	void	*mem;
 
-	if ((mem = malloc(size)) == NULL)
+	mem = malloc(size);
+	if (!mem)
 		return (NULL);
-	ft_memset(mem, 0, size);
+	ft_bzero(mem, size);
 	return (mem);
 }
